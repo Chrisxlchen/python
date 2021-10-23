1d01b01cb99425a1e27d4a18b27a2dffbcb0a140
Chris <chris.xl.chen@gmail.com>
2021-10-22 21:00:50 -0700

    turtle practice

---
+++ square_spiral1.py
@@ -0,0 +1,10 @@
+# SquareSpiral1.py
+import time
+import turtle
+t = turtle.Pen()
+t.speed(0)
+for x in range(100):
+    t.forward(2*x)
+    t.left(90)
+
+time.sleep(10)
---
+++ square_spiral2.py
@@ -0,0 +1,9 @@
+# SquareSpiral2.py
+import time
+import turtle
+t = turtle.Pen()
+t.speed(0)
+for x in range(100):
+    t.forward(2*x)
+    t.left(91)
+time.sleep(10)
